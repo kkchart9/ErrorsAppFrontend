6d80e4568f6cb00b5808c39ce132e47bbdf5f04a
kkchart9 <koudai200109@gmail.com>
2023-12-13 04:47:31 +0900

    アバウトへのリンクを追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home() {
 
     return (
         <>
             <h1>Hello,world!</h1>
+            <Link to={`/about`}>アバウトへ</Link>
+
         </>
     );
 }
